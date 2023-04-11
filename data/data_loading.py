f30cdccb5e2fcbdbc457c95d52213f4507ed36d9
luke-dinh <dinhphuoclockt@gmail.com>
2023-04-11 23:17:21 +0700

    add data folder to master branch

---
+++ data/data_loading.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Load dataset
+data_url = "https://raw.githubusercontent.com/GokuMohandas/Made-With-ML/main/datasets/projects.csv"
+df_data = pd.read_csv(data_url)
+
+# Load label
+label_url = "https://raw.githubusercontent.com/GokuMohandas/Made-With-ML/main/datasets/tags.csv"
+df_label = pd.read_csv(label_url)
+
+# Create dataset
+data_final = pd.merge(df_data, df_label, on="id")
+data_final = data_final[data_final.tag.notnull()]
+data_final.to_csv("dataset.csv")
